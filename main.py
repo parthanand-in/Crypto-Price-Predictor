80556c2e1f1c4944b0f928224b210ed9e9724511
Parth Anand <83247940+parthanand-in@users.noreply.github.com>
2021-08-22 18:47:38 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
